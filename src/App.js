1db1fd760c4a42deac7ff90c8e51d15b4b5f3919
awmills25 <awmills25@gmail.com>
2021-10-13 10:54:47 -0400

    set up axios api and methods

--- src/App.js
+++ src/App.js
@@ -1,25 +1,65 @@
+import React, { Component } from "react";
+import axios from "axios";
 import logo from './logo.svg';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+    constructor() {
+        super()
+        this.state = {
+            menuAPIData: {}
+        };
+    }
+
+    componentDidMount() {
+        console.log("App successfully loaded!");
+    }
+    componentDidUpdate() {
+        console.log("App successfully updated!");
+    }
+    loadAPI() {
+        const menu_API_URL = "https://port-3000-aincbootcampapi-ianrios529550.codeanyapp.com/api/menu/type_amount/3/15";
+
+        axios.get(menu_API_URL)
+            .then(res => {
+                // handle success
+                this.setState({
+
+                })
+                console.log(res.data);
+            })
+            .catch(function (error) {
+                // handle error
+                console.log(error);
+            })
+            .then(function () {
+                // always executed
+            });
+
+        //this line would happen right after axios api call
+    }
+
+    render() {
+
+        return (
+            <div className="App">
+                <header className="App-header">
+                    <img src={logo} className="App-logo" alt="logo" />
+                    <p>
+                        Edit <code>src/App.js</code> and save to reload.
+                    </p>
+                    <a
+                        className="App-link"
+                        href="https://reactjs.org"
+                        target="_blank"
+                        rel="noopener noreferrer"
+                    >
+                        Learn React
+                    </a>
+                </header>
+            </div>
+        );
+    }
 }
 
 export default App;
---
+++ src/pseudocode.md
@@ -0,0 +1,17 @@
+1. Generate restaurant information
+* Restaurant name: Berg Bistro
+* Restaurant address: 163 E Main St, Unit 130, Lexington, KY 40507
+* Restaurant hours: Tu-Sa 11A-3P
+
+2. Connect API with Axios
+
+3. Dynamic menu
+* Lunch
+* Choose at least 15 menu items
+* Get a unique price for each item
+* Display menu as an organism
+* Each menu item as a molecule
+ * bootstrap card or list item
+ * picture of meal, name, and price
+
+4. Must manage the React Component State
